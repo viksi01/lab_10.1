7192d0665ae803b95c8bb1ee55d967fcdcb21951
viksi01 <victoria.pyatnicka@gmail.com>
2022-12-16 00:31:58 +0200

    commit1

---
+++ UnitTest_10.1/UnitTest_10.1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_10.1/lab_10.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest101
+{
+	TEST_CLASS(UnitTest101)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		
+		}
+	};
+}
---
+++ lab_10.1/lab_10.1.cpp
@@ -0,0 +1,55 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void CreateTXT(char* fname)
+{
+	ofstream fout(fname); 
+	char ch;
+	string s;
+	do
+	{
+		cin.get(); 
+		cin.sync(); 
+		cout << "enter line: "; getline(cin, s); 
+		fout << s << endl;
+		cout << "continue? (y/n): "; cin >> ch;
+	} while (ch == 'y' || ch == 'Y');
+	cout << endl;
+}
+void PrintTXT(char* fname)
+{
+	ifstream fin(fname); 
+	string s; 
+	while (getline(fin, s)) 
+	{
+		cout << s << endl;
+	}
+	cout << endl;
+}
+int ProcessTXT(char* fname)
+{
+	ifstream fin(fname);
+	string s;
+	int k = 0;
+	while (getline(fin, s)) 
+	{ 
+		for (unsigned i = 0; i < s.length(); i++)
+			if ((s[i] == 'a' || s[i] == 'b' || s[i] == 'c') && (s[i + 1] == 'a' || s[i + 1] == 'b' || s[i + 1] == 'c')
+				&& (s[i + 2] == 'a' || s[i + 2] == 'b' || s[i + 2] == 'c'))
+				k++;
+	}
+	return k;
+}
+
+int main()
+{
+
+	char fname[100] = "t.txt"; 
+	CreateTXT(fname); 
+	PrintTXT(fname); 
+	cout << "k( 'a', 'b', 'c') = " << ProcessTXT(fname) << endl;
+	return 0;
+}
